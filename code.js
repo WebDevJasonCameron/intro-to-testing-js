80a3c3e9169e55bbf7164d4bd0739a2fa4fa538d
Jason Cameron <dev.jasoncameron@gmail.com>
2022-02-04 21:29:47 -0600

    Test and code Ex through eleven completed

--- code.js
+++ code.js
@@ -1,16 +1,38 @@
-// helloWorld function 
+// helloWorld Function
 function helloWorld() {
     return "Hello, World!";
 }
 
+// SayHello Function
+
+// Prior to Refactor
 // function sayHello(input) {
 //     if(input === "Alex") return "Hello, Alex!";
 //     else if(input === "Pat") return "Hello, Pat!"
 //     return "Hello, Jane!";
 // }
 
-
 function sayHello(input) {
     if(typeof input === 'undefined' || input === true || input === false) input = 'World'
     return "Hello, " + input + "!";
+}
+
+// isFive Function
+function isFive(input){
+    return input == 5;
+}
+
+// isEven Function
+
+// Refactor
+// function isEven(input){
+//     if(input === 2) return true;
+//     else if(input === -4) return true;
+//     return false;
+// }
+
+function isEven(input){
+    if(typeof input === 'boolean')return false;
+    return input % 2 === 0;
+
 }
--- tests.js
+++ tests.js
@@ -1,4 +1,4 @@
-// Exercise #1
+// helloWorld Function
 describe('helloWorld', function() {
     it('should be a defined function', function() {
       expect(typeof helloWorld).toBe('function');
@@ -14,7 +14,7 @@ describe('helloWorld', function() {
     });
 });
 
-// More
+// sayHello Function
 describe('sayHello', function () {
     it('should be a defined function', function () {
         expect(typeof sayHello).toBe('function');
@@ -40,16 +40,60 @@ describe('sayHello', function () {
     it('should return the string "Hello, World!" if input is false', function () {
         expect(sayHello(false)).toBe("Hello, World!")
     })
-
-
 });
 
-// Exercise #2
+// isFive Function
+describe('isFive', function () {
+    it('should be a defined function', function () {
+        expect(typeof isFive).toBe('function');
+    });
+    it('should return a boolean', function () {
+        expect(typeof isFive()).toBe('boolean');
+    });
+    it('should return true if input is 5', function () {
+        expect(isFive(5)).toBe(true);
+    });
+    it('should return true if input is "5"', function () {
+        expect(isFive("5")).toBe(true);
+    });
+});
 
-
-
-
-
+// isEven Function
+describe('isEven', function () {
+    it('should be a defined function', function () {
+        expect(typeof isEven).toBe('function');
+    });
+    it('should return a boolean', function () {
+        expect(typeof isEven()).toBe('boolean');
+    });
+    it('should return true when input is 2', function () {
+        expect(isEven(2)).toBe(true);
+    });
+    it('should return true when input is -4', function () {
+        expect(isEven(-4)).toBe(true);
+    });
+    it('should return false when input is 3', function () {
+        expect(isEven(3)).toBe(false);
+    });
+    it('should return false when input is "banana"', function () {
+        expect(isEven("banana")).toBe(false);
+    });
+    it('should return true when input is 8', function () {
+        expect(isEven(8)).toBe(true);
+    });
+    it('should return false when input is Infinity', function () {
+        expect(isEven(Infinity)).toBe(false);
+    });
+    it('should return false when input is true', function () {
+        expect(isEven(true)).toBe(false);
+    });
+    it('should return false when input is false', function () {
+        expect(isEven(false)).toBe(false);
+    });
+    it('should return false when input is not given', function () {
+        expect(isEven()).toBe(false);
+    });
+});
 
 
 
